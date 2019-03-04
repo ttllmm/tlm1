c997183357fd71a1232df60dd8c7d9ba736fcacb
tlm <568114272@qq.com>
2019-03-04 18:49:37 +0800

    Initial commit

---
+++ project/Kyro/Person.scala
@@ -0,0 +1,16 @@
+//package Kyro
+//import com.esotericsoftware.kryo.Kryo
+//object Person {
+//
+//}
+//class Person (v1:String,v2:Int) extends  Serializable {
+//  var name=v1
+//  var age=v1
+//
+//}
+//class MyKryoRegister extends MyKryoRegister {
+//  def registerClasses(kryo:Kyro): Unit ={
+//    kryo.register(classOf[Person])
+//  }
+//
+//}
